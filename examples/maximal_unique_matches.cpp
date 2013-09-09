3c608032961c2365a3d5ec0df622f5d288c95cd4
Simon Gog <simon.gog@gmail.com>
2013-09-09 22:37:43 +1000

    Fixed typo.

--- examples/maximal_unique_matches.cpp
+++ examples/maximal_unique_matches.cpp
@@ -17,7 +17,7 @@ using namespace sdsl;
    ESA 2013: 133-144.
    
    This is not a faithful implementation of the O(n log sigma) algorithm given there, since
-   operations are computed using trivial scans with all sigmna alphabet symbols. 
+   operations are computed using trivial scans with all sigma alphabet symbols. 
    However, this version probably works fine with DNA.
 */
 
